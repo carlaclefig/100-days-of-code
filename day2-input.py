01d294c3d957a03d932c12e4c04b4f793fc7f969
Carla Clemente <carlaclefig@gmail.com>
2024-02-05 17:38:54 -0500

    add notes variable input

--- day2-input.py
+++ day2-input.py
@@ -1,11 +1,11 @@
-myName = input("What's you name?:")
+myName = input("What's you name?:")  # myName is variable
 myFood = input("What's your favorite food?:")
 myMusic = input("What's your favorite music?:")
 doLive = input("Where do you live?:")
 print("Great, lest go!")
 print()
 print("You are")
-print(myName)
+print(myName)  # print result the variable
 print("You're probably hungry for")
 print(myFood)
 print("and you're definitely getting your groove on to")
