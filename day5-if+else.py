c863b82e9733142afe2874771a684a02e98d3d79
Carla Clemente <carlaclefig@gmail.com>
2024-02-05 22:35:36 -0500

    add if and else

---
+++ day5-if+else.py
@@ -0,0 +1,22 @@
+print("Marvel Moive Character Creator")
+print("--")
+print()
+spiderMan = input("Do you like 'hanging around'?:")
+# if: the variable 'spiderMan' responds 'yes' then the following is printed:
+if spiderMan == "yes":
+    print("Then you're Spider-man")
+# else: but if you answer anything other than 'yes' then the following is printed:
+else:
+    print("Then you're not Spider-man")
+print()
+Korg = input("Do you have a 'gravelly' voice?:")
+if Korg == "yes":
+    print("Aww,then you're Korg")
+else:
+    print("Aww,then you're not Korg")
+print()
+Marvel = input("Do you often fell 'Marvelous'?:")
+if Marvel == "yes":
+    print("Aha! You're Captain Marvel! Hi!")
+else:
+    print("You're not Captain Marvel either")
