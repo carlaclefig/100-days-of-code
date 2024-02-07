991114f5d185e57b8d6e13d9585c193cc7973b37
Carla Clemente <carlaclefig@gmail.com>
2024-02-07 12:31:27 -0500

    add-math-functions

---
+++ day10-math.py
@@ -0,0 +1,22 @@
+adding = 4 + 3
+print(adding)
+
+subtraction = 8 - 9
+print(subtraction)
+
+multiplication = 4 * 32
+print(multiplication)
+
+division = 50 / 5
+print(division)
+
+squared = 6**2  # exponents the 1st number raised to the 2nd number
+print(squared)
+
+modulo = (
+    45 % 2
+)  # 1er number es el dividend y el 2do es el divisor, el result is the residue
+print(modulo)
+
+divisor = 20 // 5  # 1er number es el dividend y el 2do es el quotient
+print(divisor)
