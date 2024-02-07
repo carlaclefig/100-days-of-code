f9c44c32154cc0d1c2a2feccc3d60c8f3262980d
Carla Clemente <carlaclefig@gmail.com>
2024-02-06 20:58:56 -0500

    add-if+if+if

---
+++ day7-if-if-if.py
@@ -0,0 +1,22 @@
+print("🕵️ FAKE FAN FINDER 👀")
+print("----------------------")
+anime = input("What's your favorite Anime?")
+if anime == "One piece":
+    print("Oh really?!")
+    character = input("Name me any of the characters?")
+    if character == "Nami":
+        print("You got that by pure chance.")
+        job = input("Okay then, what is her job on the ship?")
+        if job == "Navigator":
+            print("Hmph!")
+            bounty = input("What was her first bounty then?")
+            if bounty == "Ummm...":
+                print("See! FAKE One piece fan!")
+            else:
+                print("You are a true fan!")
+        else:
+            print("You are a fake fan!")
+    else:
+        print("You are a fake fan!")
+else:
+    print("You are a fake fan!")
