4eb7499788ec54443c3baa53474375c170205bdc
Carla Clemente <carlaclefig@gmail.com>
2024-02-07 14:05:48 -0500

    add function/float/int/round

--- day10-math.py
+++ day10-math.py
@@ -1,8 +1,12 @@
-# float, identifies the decimal number that the user enters
-myBill = float(input("What was the bill?:"))
-# int, Identifies the integer number that the user enters.
-numberPeople = int(input("How many people?:"))
-answer = myBill / numberPeople
-# round, shows the result only with the requested number of decimal places
-answer = round(answer, 2)
-print("You all owe", answer)
+print("Tip Calculator")
+print()
+spend = float(input("How much did you spend? $"))
+tip = int(input("What percentage do you what to tip? %"))
+people = int(input("How many people in your group? "))
+# It must be solved step by step according to the requested function.
+perctip = tip / 100
+totaltip = perctip * spend
+totalspend = totaltip + spend
+totalpeople = totalspend / people
+totalpeople = round(totalpeople, 2)
+print("You each owe $", totalpeople)
