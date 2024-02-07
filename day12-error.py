8fd75625b94e736f2df4b794aa92a34efc58333b
Carla Clemente <carlaclefig@gmail.com>
2024-02-07 15:57:42 -0500

    removed first error

--- day12-error.py
+++ day12-error.py
@@ -1,13 +1,13 @@
 print("100 Days of Code QUIZ")
 print()
-print("How many can you answer correctly? ") # first error
+print input("How many can you answer correctly? ") # first error
 ans1 = ("What language are we writing in? ")
 if ans1 == "python":
   print("Correct")
 else:
   print("Nope 🙈") # second error 
-ans2 = input("Which lesson number is this?")
-if(ans2>12):
+ans2 = input("Which lesson number is this? ")
+if ans2>12: # third error
 print("We're not quite that far yet")
 else:
   print("We've gone well past that!")
