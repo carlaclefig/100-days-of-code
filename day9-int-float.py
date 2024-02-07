168f7b9acc37d5bcdc794a540293d4897681f76d
Carla Clemente <carlaclefig@gmail.com>
2024-02-06 23:55:11 -0500

    add int, >=/<=

--- day9-int-float.py
+++ day9-int-float.py
@@ -1,7 +1,21 @@
 print("Generation Identifier")
 print()
 year = int(input("Which year were you born?"))
-if year < 1990:
+if year >= 1883 and year <= 1900:
     print("Hah, Lost Generation!")
-elif year == 1901:
+elif year >= 1901 and year <= 1927:
     print("Hah, Greatest Generation!")
+elif year >= 1928 and year <= 1945:
+    print("Hah, Silent Generation!")
+elif year >= 1946 and year <= 1964:
+    print("Hah, Baby Boomers!")
+elif year >= 1965 and year <= 1980:
+    print("Hah, Generation X!")
+elif year >= 1981 and year <= 1996:
+    print("Hah, Millennials!")
+elif year >= 1997 and year <= 2012:
+    print("Hah, Generation Z!")
+elif year >= 2013 and year <= 2024:
+    print("Hah, Generation Alpha!")
+else:
+    print("Try again!")
