561cb7429ed6ff8c6bac06f531234d8521f3ff6c
Carla Clemente <carlaclefig@gmail.com>
2024-02-06 23:31:26 -0500

    add int

---
+++ day9-int-float.py
@@ -0,0 +1,6 @@
+# int so that the code can read integers according to the conditional
+myScore = int(input("Your score:"))
+if myScore > 100000:
+    print("Winner!")
+else:
+    print("Try again 😭")
