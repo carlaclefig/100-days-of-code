5bc35de76fdeecd432d1203c0608699fef54a1cc
Carla Clemente <carlaclefig@gmail.com>
2024-02-08 11:14:57 -0500

    add if/elif/else

--- day13-qualification-project.py
+++ day13-qualification-project.py
@@ -6,3 +6,18 @@ score = float(input("Your score: "))
 
 pretotal = score / Maxscore
 total = round((pretotal * 100), 2)
+
+if total <= 100 and total >= 90:
+    print("You got", total, "% which is a A+")
+elif total <= 89 and total >= 80:
+    print("You got", total, "% which is a A-")
+elif total <= 79 and total >= 70:
+    print("You got", total, "% which is a B")
+elif total <= 69 and total >= 60:
+    print("You got", total, "% which is a C")
+elif total <= 59 and total >= 50:
+    print("You got", total, "% which is a D")
+elif total <= 49:
+    print("You got", total, "% which is a U")
+else:
+    print("Re-enter your score")
