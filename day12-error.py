01e374c27fa8bd3a6783e194c170fa215cfd161d
Carla Clemente <carlaclefig@gmail.com>
2024-02-07 19:03:35 -0500

    modifying order and spaces

--- day12-error.py
+++ day12-error.py
@@ -1,15 +1,15 @@
 print("100 Days of Code QUIZ")
 print()
-print ("How many can you answer correctly? ") # first error
-ans1 = input("What language are we writing in? ") # second error 
+print("How many can you answer correctly? ")  # first error
+ans1 = input("What language are we writing in? ")  # second error
 if ans1 == "python":
-  print("Correct")
+    print("Correct")
+    ans2 = int(input("Which lesson number is this? "))  # 4to error
+    if ans2 > 12:  # 5to error
+        print("We're not quite that far yet")
+    elif ans2 == 12:  # 6to error
+        print("That's right!")
+    else:
+        print("We've gone well past that!")
 else:
-  print("Nope 🙈") # third error 
-ans2 = int(input("Which lesson number is this? ")) # 4to error
-if ans2 > 12: # 5to error
-print("We're not quite that far yet")
-else:
-  print("We've gone well past that!")
-elif ans2 == 12: # 6to error
-  print("That's right!")
+    print("Nope")  # third error
