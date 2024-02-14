384aed9141c72e6d75d61b3fe3013418f2ff58d7
Carla Clemente <carlaclefig@gmail.com>
2024-02-13 22:29:16 -0500

    add true

--- day16-true-loop.py
+++ day16-true-loop.py
@@ -1,28 +1,11 @@
-while True:
-    print("This program is running")
-    again = input("Go again?: ")
-    if again == "no":
-        break
-print("Aww, i was having a good time 😭")
-
-counter = 0
-while True:
-    answer = int(input("Enter a number: "))
-    print("Adding it up")
-    # counter = counter + answer:
-    counter += answer
-    print("Current total is", counter)
-    another = input("Add another? ")
-    if another == "no":
-        break
-print("Bye!")
-
 print("Fill-in the blank Lyrics!")
 print("Type in the blank lyrics, see if ")
 print()
 while True:
-    print("Never going to____you up")
-    again = input(": ")
+    print("Never going to____you up.")
+    again = input("What is the missing word?: ")
     if again == "give":
         break
-print("Well done")
+    else:
+        print("Nope, try again.")
+print("Well done.")
