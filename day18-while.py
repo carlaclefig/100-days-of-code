c838e83df963a2527954295ddebe57eaa55843da
Carla Clemente <carlaclefig@gmail.com>
2024-02-23 17:48:13 -0500

    code the guess the number

--- day18-while.py
+++ day18-while.py
@@ -1,11 +1,21 @@
 print("GUESS THE NUMBER")
-print("You can choose a number from 1 to 1000, until you guess")
+print("You can choose a number from 1 to 1000, until you guess\n")
 score = 0
 while True:
-    number = input("What is your guess?: ")
-    if number <= 0 and number >= 399:
-        print("Too low.")
-    elif number <= 401 and number >= 1000:
-        print("Too high.")
-    elif number == 400:
-        print("Correct!")
+    number = int(input("What is your guess?: "))
+    if number >= 0 and number <= 399:
+        print("Too low.\n")
+        score += 1
+        continue
+    elif number >= 401 and number <= 1000:
+        print("Too high.\n")
+        score += 1
+        continue
+    if number == 400:
+        print("Correct!\n")
+        print("It took you", score, "guesses to get correct!")
+        exit()
+    else:
+        print()
+        print("Thanks for playing!")
+        exit()
