9bc4df2f0ba753d64e24da1f20aa5cb13f42a988
Carla Clemente <carlaclefig@gmail.com>
2024-02-23 18:40:40 -0500

    add for/in function to loop

--- day19-for-loop.py
+++ day19-for-loop.py
@@ -2,3 +2,9 @@ counter = 0
 while counter < 10:
     print(counter)
     counter += 1
+
+# for new variable in range (number of values):
+# new variable: crea una nueva variable
+# number of values: crea una lista de números
+for i in range(10):
+    print(i)
