6b086df16af379b2995d2e376ddc1f1a3395c0b0
Carla Clemente <carlaclefig@gmail.com>
2024-02-23 19:23:11 -0500

    example of for/in with options

--- day19-for-loop.py
+++ day19-for-loop.py
@@ -1,10 +1,18 @@
 counter = 0
-while counter < 10:
+while counter < 8:
     print(counter)
     counter += 1
 
 # for new variable in range (number of values):
 # new variable: crea una nueva variable
 # number of values: crea una lista de números
-for i in range(10):
+for i in range(5):
     print(i)
+
+# The total variable starts at 0.
+# For the new variable number in the range of 100.
+# The total will be added from 0 to 99
+total = 0
+for number in range(10):
+    total += number
+    print(total)
