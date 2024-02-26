de237d12d6824066a34b948e2bac2f1bdcfd2249
Carla Clemente <carlaclefig@gmail.com>
2024-02-26 18:23:30 -0500

    range code with a negative interval

--- day20-range.py
+++ day20-range.py
@@ -14,3 +14,6 @@ for i in range(1, 13):
 # range from 0 to 99, summed from 20 by 20
 for i in range(0, 100, 20):
     print(i)
+# range from 10 to 0, subtracting from -2 by -2
+for i in range(10, -1, -2):
+    print(i)
