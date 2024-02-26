d50ca5c3485fdd864a2f6dab5ba0b43a7e9d828a
Carla Clemente <carlaclefig@gmail.com>
2024-02-26 18:02:18 -0500

    code thirteen table with range

--- day20-range.py
+++ day20-range.py
@@ -2,6 +2,11 @@
 for i in range(200, 210):
     print(i)
 
-#
+# star 1 and ends 7
 for i in range(1, 8):
     print("Day", i)
+
+
+print("Thirteen times table")
+for i in range(1, 13):
+    print(i, "x 13 =", i * 13)
