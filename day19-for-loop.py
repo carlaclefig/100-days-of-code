132db8cdaf2eb97f6d121442bf4dbcfd292b12ce
Carla Clemente <carlaclefig@gmail.com>
2024-02-26 17:52:30 -0500

    code calculator with interest

--- day19-for-loop.py
+++ day19-for-loop.py
@@ -12,24 +12,28 @@ for i in range(5):
 # The total variable starts at 0.
 # For the new variable number in the range of 100.
 # The total will be added from 0 to 99
-total = 0
+loan_with_interest = 0
 for number in range(10):
-    total += number
-    print(total)
+    loan_with_interest += number
+    print(loan_with_interest)
 
 for days in range(7):
     print("Day", days + 1)
 
 
-print("Loan Calculator\n")
-print("$1000 over 10 years at 5% APR\n")
-total = 1000
-apr = 0.05
-int_pain = 0
+print(
+    """
+Loa Calculator
+
+$1000 over 10 years at 5% APR
+"""
+)
+loan_with_interest = 1000
+interest_rate = 0.05
+interest_per_year = 0
 for i in range(10):
-    int = total * apr
-    total += int
-    int_pain += int
-    print("Year", i + 1, "is", round(total, 2))
-print()
-print("You paid $", round(int_pain, 2), "in interest!")
+    interest_of_the_year = loan_with_interest * interest_rate
+    loan_with_interest += interest_of_the_year
+    interest_per_year += interest_of_the_year
+    print("Year", i + 1, "is", round(loan_with_interest, 2))
+print("\nYou paid $", round(interest_per_year, 2), "in interest!")
