b1e31c4b98e183237b4f52b93fc9f42110a286e5
Carla Clemente <carlaclefig@gmail.com>
2024-03-05 14:16:57 -0500

    Multiplication game using for/in

---
+++ day21-build-a-math-facts-game.py
@@ -0,0 +1,18 @@
+print("Math Game!\n")
+number_user = int(input("Name your multiples:\n "))
+score = 0
+for number in range(1, 11):
+    correct = number * number_user
+    print(number, "x", number_user, "=")
+
+    correct_answer = int(input("> "))
+    if correct_answer == correct:
+        print("Great work")
+        score += 1
+    else:
+        print("Nope. The answer was", correct)
+
+    if score == 10:
+        print("Wow! A perfect score! 🥳", score)
+    else:
+        print("You scored", score, "out of 10")
