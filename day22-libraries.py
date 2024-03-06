2de05bcd98e4e9e8589a01f9f0dc7dcd30dc83c5
Carla Clemente <carlaclefig@gmail.com>
2024-03-05 22:42:01 -0500

    code use of random with a number guessing game

--- day22-libraries.py
+++ day22-libraries.py
@@ -1,5 +1,25 @@
 import random
 
-for i in range(10):
-    number = random.randint(1, 100)
-    print(number)
+print("GUESS THE NUMBER")
+print("You can choose a number from 1 to 100, until you guess\n")
+
+number = random.randint(1, 100)
+score = 0
+print(number)
+while True:
+    number_user = int(input("What is your guess?: "))
+    score += 1
+    if number_user < 0:
+        print(
+            "You have entered a negative number , which is the secret way to exit the game."
+        )
+        exit()
+
+    if number_user > number:
+        print("Too high.\n")
+    elif number_user < number:
+        print("Too low.\n")
+    elif number_user == number:
+        print("Correct!\n")
+        print("It took you", score, "guesses to get correct!")
+        exit()
