dd1c1be21cd188afc5c2dd9e8e4864647b5534bd
Carla Clemente <carlaclefig@gmail.com>
2024-03-14 17:40:23 -0500

    more argument options have been added to the code

--- day24-parameters.py
+++ day24-parameters.py
@@ -1,11 +1,20 @@
 # In a subroutine, the () are for the argument (FYI argument is another word for parameter). These are the pieces of information we pass to the code. These can be variable names that are made up for the first time within the argument ().
-def which_cake(ingredient):
+def which_cake(ingredient, base, coating):
     if ingredient == "chocolate":
         print("Mmm, chocolate cake is amazing")
     elif ingredient == "broccoli":
         print("You what mate?!")
     else:
         print("Yeah, that's great I suppose...")
+        print(
+            "So you want a",
+            ingredient,
+            "cake on a",
+            base,
+            "base with",
+            coating,
+            "on top?",
+        )
 
 
-which_cake("chocolate")
+which_cake("carrot", "biscuit", "icing")
