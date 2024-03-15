b293c6fe54719d5f75737e89491d826ec014d997
Carla Clemente <carlaclefig@gmail.com>
2024-03-14 19:04:03 -0500

    choose your topping your pizza

--- day24-parameters.py
+++ day24-parameters.py
@@ -1,4 +1,11 @@
 # In a subroutine, the () are for the argument (FYI argument is another word for parameter). These are the pieces of information we pass to the code. These can be variable names that are made up for the first time within the argument ().
+print("INGREDIENTS FOR A CAKE")
+
+user_ingredient = input("Name an ingredient: ")
+user_base = input("Name a type of base: ")
+user_coating = input("Fave cake topping: ")
+
+
 def which_cake(ingredient, base, coating):
     if ingredient == "chocolate":
         print("Mmm, chocolate cake is amazing")
@@ -6,23 +13,22 @@ def which_cake(ingredient, base, coating):
         print("You what mate?!")
     else:
         print("Yeah, that's great I suppose...")
-        print(
-            "So you want a",
-            ingredient,
-            "cake on a",
-            base,
-            "base with",
-            coating,
-            "on top?",
-        )
 
-
-user_ingredient = input("Name an ingredient: ")
-user_base = input("Name a type of base: ")
-user_coating = input("Fave cake topping: ")
+    print(
+        "So you want a",
+        ingredient,
+        "cake on a",
+        base,
+        "base with",
+        coating,
+        "on top?",
+    )
 
 
 which_cake(user_ingredient, user_base, user_coating)
+print()
+
+print("BIGGER NUMBER")
 
 
 def biggerNumber(number1, number2):
@@ -33,3 +39,21 @@ def biggerNumber(number1, number2):
 
 
 biggerNumber(18, 332)
+print()
+
+print("CHOOSE YOUR PIZZA")
+topping1 = input("Name a pizza topping: ")
+topping2 = input("Name a second pizza topping: ")
+
+
+def pizza_order(topping1, topping2):
+    if topping1 == "pepperoni":
+        print(topping1, "is a great choice")
+    else:
+        print(topping1, "is kinda lame on pizza")
+
+    print(topping2, "sounds delicious, much better than", topping1)
+
+
+pizza_order(topping1, topping2)
+print()
