1c35f7485e1fee4a911307cccbbb7ffc20ea317f
Carla Clemente <carlaclefig@gmail.com>
2024-04-02 18:00:26 -0500

    add the time option

--- day26-more-libraries.py
+++ day26-more-libraries.py
@@ -1,10 +1,15 @@
 # ---------------------------------- Example 01 ---------------------------------------
-import os  # import OS module
+import os, time  # import OS and time module
 
 print("Welcome")
 print("to")
 print("Replit")  # Print the first 3 lines
+
+time.sleep(1)  # generates an erase after "1" second
 os.system("clear")  # Instantly calls the operating system's "clear" command,
 # which is used to clear the terminal screen
 username = input("Username: ")
 # Print "Username" after clearing everything in the terminal
+
+
+# ---------------------------------- Exercise 01 ---------------------------------------
