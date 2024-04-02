057f8c55d9d0837aaa8122f46b958fac0b35f69f
Carla Clemente <carlaclefig@gmail.com>
2024-04-02 17:28:10 -0500

    OS/ Clear code notes

--- day26-more-libraries.py
+++ day26-more-libraries.py
@@ -1,8 +1,10 @@
 # ---------------------------------- Example 01 ---------------------------------------
-import os
+import os  # import OS module
 
 print("Welcome")
 print("to")
-print("Replit")
-os.system("clear")
+print("Replit")  # Print the first 3 lines
+os.system("clear")  # Instantly calls the operating system's "clear" command,
+# which is used to clear the terminal screen
 username = input("Username: ")
+# Print "Username" after clearing everything in the terminal
