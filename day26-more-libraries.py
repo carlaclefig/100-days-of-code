d3ef05a84bc274436bf2245d0852adc294be9aaa
Carla Clemente <carlaclefig@gmail.com>
2024-04-01 22:00:50 -0500

    used import os

---
+++ day26-more-libraries.py
@@ -0,0 +1,8 @@
+# ---------------------------------- Example 01 ---------------------------------------
+import os
+
+print("Welcome")
+print("to")
+print("Replit")
+os.system("clear")
+username = input("Username: ")
