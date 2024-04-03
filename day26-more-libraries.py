cbbea2b8fe7f9a797bb7d1d1b0ff66380e179246
Carla Clemente <carlaclefig@gmail.com>
2024-04-03 13:19:41 -0500

    code to play an audio file by time

--- day26-more-libraries.py
+++ day26-more-libraries.py
@@ -13,3 +13,33 @@ username = input("Username: ")
 
 
 # ---------------------------------- Exercise 01 ---------------------------------------
+from replit import audio
+import os, time
+
+print(" 🎶 MyPOD Music Player 🎶\n")
+
+print("Press 1 to play")
+time.sleep(2)
+print("Press 2 to exit")
+time.sleep(2)
+print("Press anything else to see the menu again")
+time.sleep(2)
+os.system("clear")
+
+
+def play():
+    source = audio.play_file("audio.wav")
+    source.paused = False
+
+
+while True:
+    player = input("Select an option > ")
+    if player == "1":
+        play()
+        print("Listen to this")
+        time.sleep(10)
+    elif player == "2":
+        break
+    else:
+        player = input("Select an option > ")
+        continue
